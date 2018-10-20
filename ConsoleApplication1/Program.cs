cd558d6763ae96c678ff3a2bee04392fa8e2ca1e
yudinaMarina <43365617+yudinaMarina@users.noreply.github.com>
2018-10-20 15:39:30 +0500

    Модульное тестирование

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -6,32 +6,43 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication1
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)//2
-        {           
-            double Ru = 0;
+        {
+            string Ru = "";
             string line = Console.ReadLine().ToUpper();
+            //if (line == "")
+            //{
+            //    Console.WriteLine("Введите текст");
+            //    line = Console.ReadLine().ToUpper();
+            //    //return line;
+            //}
             Ru = Opred_Cyr(line);
-            if (Ru >.5)
+            if (line=="true")
             {
                 Console.WriteLine("данный текст является предложением русского языка");
             }
             else Console.WriteLine("данный текст не является предложением русского языка");
             Console.ReadKey();
         }
-
-        
+     
         private static char[] cyrillics()//1
         {
             string cyr = "абвгдеёжзийклмнопрстуфхцчшщъыьэюя";
             return (cyr + cyr.ToUpper()).ToCharArray();
         }
 
-        private static double Opred_Cyr(string line)//1
+        public static string Opred_Cyr(string line)//1
         {
             char[] cyr = cyrillics();
-            return line.Sum(c=>cyr.Contains(c)?1.0:0.0) / (double)line.Length;
+            if ((line.Sum(c => cyr.Contains(c) ? 1.0 : 0.0) / (double)line.Length) > .5)
+            {
+                line="true";
+            }
+            else line = "false";
+            return line;
         }
     }
 }
+
---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApplication1;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string line = "Привет";
+            string expected = "true";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected,actual);
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            string line = "gfghhkyk";
+            string expected = "false";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            string line = "Привет hjfj";
+            string expected = "true";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            string line = "gfghhkyk попророп";
+            string expected = "false";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod5()
+        {
+            string line = "12312343155";
+            string expected = "false";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod6()
+        {
+            string line = "!./,@";
+            string expected = "false";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void TestMethod7()
+        {
+            string line = "";
+            string expected = "false";
+            string actual = Program.Opred_Cyr(line);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
